6e1860ba289b2f3f25afca0c8ac39ace37e870c9
franklin rodriguez <franklinserif@gmail.com>
2022-04-27 01:34:17 -0400

    Styles and structure for NewBudget component

--- src/components/NewBudget.jsx
+++ src/components/NewBudget.jsx
@@ -3,7 +3,22 @@
  * @returns {JSX.Element} <NewBudget/>
  */
 const NewBudget = () => {
-  return <div>NewBudget</div>;
+  return (
+    <div className="contenedor-presupuesto contenedor sombra">
+      <form className="formulario">
+        <div className="campo">
+          <label htmlFor="presupuesto">Definir Presupuesto</label>
+          <input
+            className="nuevo-presupuesto"
+            type="text"
+            placeholder="Añade tu presupuesto"
+          />
+        </div>
+
+        <input type="submit" value="Añadir" />
+      </form>
+    </div>
+  );
 };
 
 export default NewBudget;
