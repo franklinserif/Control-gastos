cafd10614dfc8d32af42a56f242552b31661ee67
franklin rodriguez <franklinserif@gmail.com>
2022-04-27 01:59:50 -0400

    Consume the BudgetContext in NewBudget component
    Consume the BudgetContext and get the global state budget
    setBudget function to change the state of budget in NewBudget component

--- src/components/NewBudget.jsx
+++ src/components/NewBudget.jsx
@@ -1,8 +1,18 @@
+import { useContext } from 'react';
+import { BudgetContext } from '../context/BudgetProvider';
+
 /**
  * New Buget component
  * @returns {JSX.Element} <NewBudget/>
  */
 const NewBudget = () => {
+  /**
+   * Get the budget state value and set Function
+   * to handle the global budget state from
+   * BudgetContext provide by BudgetProvider component
+   */
+  const { budget, setBudget } = useContext(BudgetContext);
+
   return (
     <div className="contenedor-presupuesto contenedor sombra">
       <form className="formulario">
