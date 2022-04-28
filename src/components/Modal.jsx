2ab4da7eac328bc4850f0af3145dd86d657ce053
franklin rodriguez <franklinserif@gmail.com>
2022-04-27 22:30:09 -0400

    Button added in modal with icon

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,13 @@
+import IconCloseModal from '../assets/img/cerrar.svg';
+
 const Modal = () => {
-  return <div className="modal">Modal</div>;
+  return (
+    <div className="modal">
+      <button className="cerrar-modal">
+        <img src={IconCloseModal} alt="icono cerrar modal" />
+      </button>
+    </div>
+  );
 };
 
 export default Modal;
