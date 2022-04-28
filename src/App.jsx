a3d8989317c95ce4e41dab6e4edce0bf9823fdbc
franklin rodriguez <franklinserif@gmail.com>
2022-04-27 22:13:37 -0400

    div.nuevo-gastos element added in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import { useContext } from 'react';
+import { BudgetContext } from './context/BudgetProvider';
+import IconNewExpense from './assets/img/nuevo-gasto.svg';
 import Header from './components/Header';
+
 /**
  * Componente App, here will
  * be all the app structure
  * @returns {JSX.Element} <App/>
  */
 function App() {
+  const { isBudgetValid } = useContext(BudgetContext);
+
   return (
     <div>
       <Header />
+
+      {/** If the budget had a correct value, isBudget will be true
+       * and React will render div.nuevo-gastos element
+       */}
+      {isBudgetValid && (
+        <div className="nuevo-gasto">
+          <img src={IconNewExpense} alt="Icono nuevo gasto" />
+        </div>
+      )}
     </div>
   );
 }
