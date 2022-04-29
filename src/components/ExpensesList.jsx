516e1efb78883dd773495003f90eefa1163d82e8
franklin rodriguez <franklinserif@gmail.com>
2022-04-29 01:06:48 -0400

    Added List of expense with Expense component in ExpenseList

--- src/components/ExpensesList.jsx
+++ src/components/ExpensesList.jsx
@@ -1,9 +1,14 @@
 import PropTypes from 'prop-types';
+import Expense from './Expense';
 
 const ExpensesList = ({ expenses }) => {
   return (
     <div className="listado-gastos contenedor">
       {expenses.length ? 'Gastos' : 'No hay gastos aun'}
+
+      {expenses.map((expense) => (
+        <Expense key={expense.id} expense={expense} />
+      ))}
     </div>
   );
 };
