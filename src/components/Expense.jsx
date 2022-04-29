96b61c137b378cf130d21b40199d4efd26551906
franklin rodriguez <franklinserif@gmail.com>
2022-04-29 01:04:47 -0400

    Expense component create

---
+++ src/components/Expense.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+const Expense = ({ expense }) => {
+  return <div>Expense</div>;
+};
+
+Expense.propTypes = {
+  expense: PropTypes.object,
+};
+
+export default Expense;
